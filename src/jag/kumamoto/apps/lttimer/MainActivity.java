a972d93e05edf3c9ebfdd952a787e40d5d4b840d
M.Nakamura <zmf42190@gmail.com>
2013-08-22 20:25:59 +0900

    1.0.1.1

--- src/jag/kumamoto/apps/lttimer/MainActivity.java
+++ src/jag/kumamoto/apps/lttimer/MainActivity.java
@@ -8,43 +8,64 @@
  */
 package jag.kumamoto.apps.lttimer;
 
+import java.util.Locale;
+
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 
-@SuppressLint("DefaultLocale")
 public class MainActivity extends Activity {
 	private static final String Tag = "MainActivity";
 	private static final String mTimerFormat = "%d:%02d";
-	private static final int mPeriod = 5;	// minutes
+	private static final int mPeriod = 300; // sec
+	private static final int mPeriodPreAlarm = 30; // sec
+	private boolean mTimerEnable = false;
 	private CountDownTimer mCountDownTimer = null;
 	private Ringtone mRingtone = null;
+	private Handler mHandler = new Handler();
+	private Runnable mRunnable = new Runnable() {
+		public void run() {
+			Log.i(Tag, "run");
+			try {
+				if (!mRingtone.isPlaying()) {
+					// アラームを鳴らす
+					mRingtone.play();
+					mTimerEnable = false;
+				}
+				// 2回鳴らす
+				if (mTimerEnable)
+					mHandler.postDelayed(mRunnable, 100);
+			} catch (Exception e) {
+				Log.e(Tag, e.getMessage());
+			}
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		Log.i(Tag, "onCreate");
 		super.onCreate(savedInstanceState);
 		try {
+			// 時間を表示
 			setContentView(R.layout.activity_main);
 			TextView textView_timer = (TextView) findViewById(R.id.textView_timer);
-			String timer_text = String.format(mTimerFormat, mPeriod, 0);
+			String timer_text = String.format(Locale.US, mTimerFormat,
+					(int) (mPeriod / 60), mPeriod % 60);
 			textView_timer.setText(timer_text);
 
-			Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-	        mRingtone = RingtoneManager.getRingtone(getApplicationContext(), uri);
-			
-	        //キャッチされない例外により、スレッドが突然終了したときや、  
-	        //このスレッドに対してほかにハンドラが定義されていないときに  
-	        //呼び出されるデフォルトのハンドラを設定します。  
-	        Thread.setDefaultUncaughtExceptionHandler(new ErrorReportClass(this));  
-	    } catch (Exception e) {
-			Log.e(Tag, "onCreate");
+			// キャッチされない例外により、スレッドが突然終了したときや、
+			// このスレッドに対してほかにハンドラが定義されていないときに
+			// 呼び出されるデフォルトのハンドラを設定します。
+			Thread.setDefaultUncaughtExceptionHandler(new ErrorReportClass(this));
+		} catch (Exception e) {
 			ErrorReportClass.LogException(this, e);
 		}
 	}
@@ -52,43 +73,66 @@ public class MainActivity extends Activity {
 	public void onClickStartButton(View view) {
 		Log.i(Tag, "onClickOkButton");
 		try {
-			// カウントダウンする
-			mCountDownTimer = new CountDownTimer(mPeriod * 60000, 1000) {
+			// Keep screen on
+			getWindow()
+					.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+			// 通知音を取得
+			Uri uri = RingtoneManager
+					.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+			mRingtone = RingtoneManager.getRingtone(getApplicationContext(),
+					uri);
+
+			// タイマーの準備
+			mCountDownTimer = new CountDownTimer(mPeriod * 1000, 100) {
 				TextView textView_timer = (TextView) findViewById(R.id.textView_timer);
 
 				// カウントダウン処理
 				public void onTick(long millisUntilFinished) {
 					try {
-						String timer_text = String.format(mTimerFormat,
-								millisUntilFinished / 60000,
-								(millisUntilFinished / 1000) % 60);
+						// 時間を再表示
+						String timer_text = String.format(Locale.US,
+								mTimerFormat,
+								(int) (millisUntilFinished / 60000),
+								(int) (millisUntilFinished / 1000) % 60);
 						textView_timer.setText(timer_text);
-						if((int)(millisUntilFinished/1000) == 30){
-							mRingtone.play();
+
+						if ((int) (millisUntilFinished / 1000) == mPeriodPreAlarm) {
+							// 予鈴を鳴らす
+							if (!mRingtone.isPlaying())
+								mRingtone.play();
 						}
 					} catch (Exception e) {
-						Log.e(Tag, "onTick");
 						ErrorReportClass.LogException(MainActivity.this, e);
 					}
 				}
 
 				// カウントが0になった時の処理
 				public void onFinish() {
+					Log.i(Tag, "onFinish");
 					try {
-						String timer_text = String.format(mTimerFormat, 0, 0);
+						String timer_text = String.format(Locale.US,
+								mTimerFormat, 0, 0);
 						TextView textView_timer = (TextView) findViewById(R.id.textView_timer);
 						textView_timer.setText(timer_text);
 
+						// アラームを鳴らす
 						mRingtone.play();
+						// 2回鳴らす
+						mHandler.postDelayed(mRunnable, 100);
+						// Keep screen off
+						getWindow().clearFlags(
+								WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 					} catch (Exception e) {
-						Log.e(Tag, "onFinish");
 						ErrorReportClass.LogException(MainActivity.this, e);
 					}
 				}
 			};
+
+			// タイマーの開始
 			mCountDownTimer.start();
+			mTimerEnable = true;
 		} catch (Exception e) {
-			Log.e(Tag, "onClickStartButton");
 			ErrorReportClass.LogException(this, e);
 		}
 	}
@@ -96,21 +140,29 @@ public class MainActivity extends Activity {
 	public void onClickStopButton(View view) {
 		Log.i(Tag, "onClickOkButton");
 		try {
+			mTimerEnable = false;
+			// Keep screen off
+			getWindow().clearFlags(
+					WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+			// タイマーの停止
 			if (mCountDownTimer != null)
 				mCountDownTimer.cancel();
 			mCountDownTimer = null;
+
+			// 時間を再表示
 			TextView textView_timer = (TextView) findViewById(R.id.textView_timer);
-			String timer_text = String.format(mTimerFormat, mPeriod, 0);
+			String timer_text = String.format(Locale.US, mTimerFormat,
+					(int) (mPeriod / 60), mPeriod % 60);
 			textView_timer.setText(timer_text);
 		} catch (Exception e) {
-			Log.e(Tag, "onClickStopButton");
 			ErrorReportClass.LogException(this, e);
 		}
 	}
-	  
-    @Override  
-    protected void onStart() {  
-        super.onStart();  
-        ErrorReportClass.SendBugReportDialog(this.getApplicationContext());  
-    }
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// 前回異常終了していたとき、エラーレポートを送信する
+		ErrorReportClass.SendBugReportDialog(this.getApplicationContext());
+	}
 }
